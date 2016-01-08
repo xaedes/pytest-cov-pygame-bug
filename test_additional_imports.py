76b22e1f70937ca179222c33295a2602dc4d2951
xaedes <xaedes@gmail.com>
2016-01-08 01:03:07 +0100

    added imports that where imported from not passing test
    doesn't make it reproduce the error

---
+++ test_additional_imports.py
@@ -0,0 +1,850 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+try:
+    import ConfigParser
+except:
+    pass
+try:
+    import _json
+except:
+    pass
+try:
+    import _multiprocessing
+except:
+    pass
+try:
+    import _socket
+except:
+    pass
+try:
+    import _ssl
+except:
+    pass
+try:
+    import base64
+except:
+    pass
+try:
+    import calendar
+except:
+    pass
+try:
+    import copy
+except:
+    pass
+try:
+    import coverage
+except:
+    pass
+try:
+    import coverage.ConfigParser
+except:
+    pass
+try:
+    import coverage.annotate
+except:
+    pass
+try:
+    import coverage.atexit
+except:
+    pass
+try:
+    import coverage.backward
+except:
+    pass
+try:
+    import coverage.bytecode
+except:
+    pass
+try:
+    import coverage.cPickle
+except:
+    pass
+try:
+    import coverage.cStringIO
+except:
+    pass
+try:
+    import coverage.cmdline
+except:
+    pass
+try:
+    import coverage.codecs
+except:
+    pass
+try:
+    import coverage.collections
+except:
+    pass
+try:
+    import coverage.collector
+except:
+    pass
+try:
+    import coverage.config
+except:
+    pass
+try:
+    import coverage.control
+except:
+    pass
+try:
+    import coverage.coverage
+except:
+    pass
+try:
+    import coverage.data
+except:
+    pass
+try:
+    import coverage.datetime
+except:
+    pass
+try:
+    import coverage.debug
+except:
+    pass
+try:
+    import coverage.dis
+except:
+    pass
+try:
+    import coverage.encodings
+except:
+    pass
+try:
+    import coverage.env
+except:
+    pass
+try:
+    import coverage.errno
+except:
+    pass
+try:
+    import coverage.execfile
+except:
+    pass
+try:
+    import coverage.files
+except:
+    pass
+try:
+    import coverage.fnmatch
+except:
+    pass
+try:
+    import coverage.glob
+except:
+    pass
+try:
+    import coverage.hashlib
+except:
+    pass
+try:
+    import coverage.html
+except:
+    pass
+try:
+    import coverage.imp
+except:
+    pass
+try:
+    import coverage.importlib
+except:
+    pass
+try:
+    import coverage.inspect
+except:
+    pass
+try:
+    import coverage.io
+except:
+    pass
+try:
+    import coverage.json
+except:
+    pass
+try:
+    import coverage.keyword
+except:
+    pass
+try:
+    import coverage.locale
+except:
+    pass
+try:
+    import coverage.marshal
+except:
+    pass
+try:
+    import coverage.misc
+except:
+    pass
+try:
+    import coverage.monkey
+except:
+    pass
+try:
+    import coverage.multiprocessing
+except:
+    pass
+try:
+    import coverage.ntpath
+except:
+    pass
+try:
+    import coverage.opcode
+except:
+    pass
+try:
+    import coverage.optparse
+except:
+    pass
+try:
+    import coverage.os
+except:
+    pass
+try:
+    import coverage.parser
+except:
+    pass
+try:
+    import coverage.phystokens
+except:
+    pass
+try:
+    import coverage.pipes
+except:
+    pass
+try:
+    import coverage.platform
+except:
+    pass
+try:
+    import coverage.plugin
+except:
+    pass
+try:
+    import coverage.plugin_support
+except:
+    pass
+try:
+    import coverage.posixpath
+except:
+    pass
+try:
+    import coverage.python
+except:
+    pass
+try:
+    import coverage.pytracer
+except:
+    pass
+try:
+    import coverage.random
+except:
+    pass
+try:
+    import coverage.re
+except:
+    pass
+try:
+    import coverage.report
+except:
+    pass
+try:
+    import coverage.results
+except:
+    pass
+try:
+    import coverage.shlex
+except:
+    pass
+try:
+    import coverage.shutil
+except:
+    pass
+try:
+    import coverage.socket
+except:
+    pass
+try:
+    import coverage.summary
+except:
+    pass
+try:
+    import coverage.sys
+except:
+    pass
+try:
+    import coverage.templite
+except:
+    pass
+try:
+    import coverage.textwrap
+except:
+    pass
+try:
+    import coverage.threading
+except:
+    pass
+try:
+    import coverage.time
+except:
+    pass
+try:
+    import coverage.token
+except:
+    pass
+try:
+    import coverage.tokenize
+except:
+    pass
+try:
+    import coverage.traceback
+except:
+    pass
+try:
+    import coverage.tracer
+except:
+    pass
+try:
+    import coverage.types
+except:
+    pass
+try:
+    import coverage.version
+except:
+    pass
+try:
+    import coverage.xml
+except:
+    pass
+try:
+    import coverage.xmlreport
+except:
+    pass
+try:
+    import coverage.zipimport
+except:
+    pass
+try:
+    import dis
+except:
+    pass
+try:
+    import email
+except:
+    pass
+try:
+    import email.Charset
+except:
+    pass
+try:
+    import email.Encoders
+except:
+    pass
+try:
+    import email.Errors
+except:
+    pass
+try:
+    import email.FeedParser
+except:
+    pass
+try:
+    import email.Generator
+except:
+    pass
+try:
+    import email.Header
+except:
+    pass
+try:
+    import email.Iterators
+except:
+    pass
+try:
+    import email.MIMEAudio
+except:
+    pass
+try:
+    import email.MIMEBase
+except:
+    pass
+try:
+    import email.MIMEImage
+except:
+    pass
+try:
+    import email.MIMEMessage
+except:
+    pass
+try:
+    import email.MIMEMultipart
+except:
+    pass
+try:
+    import email.MIMENonMultipart
+except:
+    pass
+try:
+    import email.MIMEText
+except:
+    pass
+try:
+    import email.Message
+except:
+    pass
+try:
+    import email.Parser
+except:
+    pass
+try:
+    import email.Utils
+except:
+    pass
+try:
+    import email._parseaddr
+except:
+    pass
+try:
+    import email.base64
+except:
+    pass
+try:
+    import email.base64MIME
+except:
+    pass
+try:
+    import email.base64mime
+except:
+    pass
+try:
+    import email.binascii
+except:
+    pass
+try:
+    import email.cStringIO
+except:
+    pass
+try:
+    import email.calendar
+except:
+    pass
+try:
+    import email.charset
+except:
+    pass
+try:
+    import email.codecs
+except:
+    pass
+try:
+    import email.email
+except:
+    pass
+try:
+    import email.encoders
+except:
+    pass
+try:
+    import email.errors
+except:
+    pass
+try:
+    import email.feedparser
+except:
+    pass
+try:
+    import email.iterators
+except:
+    pass
+try:
+    import email.message
+except:
+    pass
+try:
+    import email.mime
+except:
+    pass
+try:
+    import email.os
+except:
+    pass
+try:
+    import email.parser
+except:
+    pass
+try:
+    import email.quopri
+except:
+    pass
+try:
+    import email.quopriMIME
+except:
+    pass
+try:
+    import email.quoprimime
+except:
+    pass
+try:
+    import email.random
+except:
+    pass
+try:
+    import email.re
+except:
+    pass
+try:
+    import email.socket
+except:
+    pass
+try:
+    import email.string
+except:
+    pass
+try:
+    import email.sys
+except:
+    pass
+try:
+    import email.time
+except:
+    pass
+try:
+    import email.urllib
+except:
+    pass
+try:
+    import email.utils
+except:
+    pass
+try:
+    import email.uu
+except:
+    pass
+try:
+    import email.warnings
+except:
+    pass
+try:
+    import encodings.ascii
+except:
+    pass
+try:
+    import gc
+except:
+    pass
+try:
+    import gettext
+except:
+    pass
+try:
+    import glob
+except:
+    pass
+try:
+    import importlib
+except:
+    pass
+try:
+    import importlib.sys
+except:
+    pass
+try:
+    import inspect
+except:
+    pass
+try:
+    import json
+except:
+    pass
+try:
+    import json._json
+except:
+    pass
+try:
+    import json.decoder
+except:
+    pass
+try:
+    import json.encoder
+except:
+    pass
+try:
+    import json.json
+except:
+    pass
+try:
+    import json.re
+except:
+    pass
+try:
+    import json.scanner
+except:
+    pass
+try:
+    import json.struct
+except:
+    pass
+try:
+    import json.sys
+except:
+    pass
+try:
+    import locale
+except:
+    pass
+try:
+    import marshal
+except:
+    pass
+try:
+    import multiprocessing
+except:
+    pass
+try:
+    import multiprocessing._multiprocessing
+except:
+    pass
+try:
+    import multiprocessing.atexit
+except:
+    pass
+try:
+    import multiprocessing.itertools
+except:
+    pass
+try:
+    import multiprocessing.multiprocessing
+except:
+    pass
+try:
+    import multiprocessing.os
+except:
+    pass
+try:
+    import multiprocessing.process
+except:
+    pass
+try:
+    import multiprocessing.signal
+except:
+    pass
+try:
+    import multiprocessing.subprocess
+except:
+    pass
+try:
+    import multiprocessing.sys
+except:
+    pass
+try:
+    import multiprocessing.threading
+except:
+    pass
+try:
+    import multiprocessing.util
+except:
+    pass
+try:
+    import multiprocessing.weakref
+except:
+    pass
+try:
+    import ntpath
+except:
+    pass
+try:
+    import opcode
+except:
+    pass
+try:
+    import optparse
+except:
+    pass
+try:
+    import parser
+except:
+    pass
+try:
+    import pickle
+except:
+    pass
+try:
+    import pipes
+except:
+    pass
+try:
+    import pkg_resources
+except:
+    pass
+try:
+    import pkg_resources._vendor
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging.__about__
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging._compat
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging._structures
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging.specifiers
+except:
+    pass
+try:
+    import pkg_resources._vendor.packaging.version
+except:
+    pass
+try:
+    import pkgutil
+except:
+    pass
+try:
+    import platform
+except:
+    pass
+try:
+    import plistlib
+except:
+    pass
+try:
+    import quopri
+except:
+    pass
+try:
+    import select
+except:
+    pass
+try:
+    import shlex
+except:
+    pass
+try:
+    import socket
+except:
+    pass
+try:
+    import ssl
+except:
+    pass
+try:
+    import subprocess
+except:
+    pass
+try:
+    import symbol
+except:
+    pass
+try:
+    import textwrap
+except:
+    pass
+try:
+    import token
+except:
+    pass
+try:
+    import tokenize
+except:
+    pass
+try:
+    import urllib
+except:
+    pass
+try:
+    import urlparse
+except:
+    pass
+try:
+    import uu
+except:
+    pass
+try:
+    import xml
+except:
+    pass
+try:
+    import xml.dom
+except:
+    pass
+try:
+    import xml.dom.NodeFilter
+except:
+    pass
+try:
+    import xml.dom.copy
+except:
+    pass
+try:
+    import xml.dom.domreg
+except:
+    pass
+try:
+    import xml.dom.minicompat
+except:
+    pass
+try:
+    import xml.dom.minidom
+except:
+    pass
+try:
+    import xml.dom.xml
+except:
+    pass
+try:
+    import xml.dom.xmlbuilder
+except:
+    pass
+try:
+    import zipfile
+except:
+    pass
+try:
+    import zlib
+except:
+    pass
+
+
+from draw import draw
+
+import pygame
+import numpy as np
+
+def test_draw():
+    screen = draw()
+    reference = pygame.image.load("image.png")
+
+    pygame.image.save(screen, "image_test.png")    
+
+    np.testing.assert_almost_equal(
+        pygame.surfarray.pixels3d(screen),
+        pygame.surfarray.pixels3d(reference)
+        )
+
+    print "test ok"
+    
+def main():
+    test_draw()
+
+try:
+    if __name__ == '__main__':
+        main()
+finally:
+    pass
+    import pprint, sys
+    pprint.pprint(sys.path)
+    pprint.pprint(sys.modules)
+
